ea63b47672efe22b270ef0bc00f4c1a6c8c96fdf
CARLOSMOREIRADOSSANT <G00429041@atu.ie>
2025-03-18 10:07:27 +0000

    CalculatorTest Add Completed

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Calculator {
+
+    public int add(int firstNum, int secondNum)
+    {
+        long result = (long) firstNum + secondNum;
+        if(result > Integer.MAX_VALUE)
+        {
+            throw new IllegalArgumentException("This value is too big");
+
+        }
+        return (int) result;
+    }
+}
